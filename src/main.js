a082d3b022c7ce7628af4286b163076c533c8379
Geoff Goetz and Joseph Wangemann <geoff@>
2020-02-12 15:28:25 -0800

    Change API to pull master list.

--- src/main.js
+++ src/main.js
@@ -2,24 +2,44 @@ import $ from 'jquery';
 import 'bootstrap';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './styles.css';
-// import { Farm } from './person';
 import { StrainService } from './strain-service';
 
-function showStrains(array) {
-  for (let i=0; i<=array.length; i++) {
-    $("#output").append(array[i].name + ", ");
-  }
-}
+// function showStrains(array, xrace, xflavor) {
+//   for (let i=0; i<array.length; i++) {
+//       console.log(array[i]);
+//       console.log(xflavor);
+
+//       console.log(xrace);
+    
+//   }
+// }
+
+// function objToArray(obj) {
+//   return (obj);
+// }
+
 
 
 $(document).ready(function() {
-  $("#flavorlist").submit(function() {
+  $("form").submit(function() {
     event.preventDefault();
-    let currentFlav = $("#selectflavor option:selected").text();
+    // let currentRace = $("#selectrace").val();
+    // let currentFlav = $("#selectflavor option:selected").text();
     (async () => {
       let strainService = new StrainService();
-      const response = await strainService.getStrainbyFlavor(currentFlav);
-      showStrains(response);
+      const response = await strainService.getStrain();
+      console.log(typeof(response));
+      console.log(response);
+      console.log(objToArray(response));
+      // let dataArray = [];
+      // let keyNames = Object.keys(response);
+      // for(let i = 0; i < keyNames.length; i++) {
+      //   dataArray.push(response[keyNames[i]]);
+      // }
+      // console.log(dataArray);
+
+      showStrains(response, currentRace, currentFlav);
+         
     })();
   });
 });
--- src/strain-service.js
+++ src/strain-service.js
@@ -1,7 +1,7 @@
 export class StrainService {
-  async getStrainbyFlavor(flavor) {
+  async getStrain() {
     try {
-      let response = await fetch(`http://strainapi.evanbusse.com/${process.env.FLAVOR_KEY}/strains/search/flavor/${flavor}`);
+      let response = await fetch(`http://strainapi.evanbusse.com/${process.env.API_KEY}/strains/search/all`);
       let jsonifiedResponse = await response.json();
       return jsonifiedResponse;
     } catch(error) {
