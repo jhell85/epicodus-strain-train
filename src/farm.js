86bb9c42eb18edb0c2bdb55fb998d5f009fb835f
Geoff Goetz and Joseph Wangemann <joseph@>
2020-02-12 11:40:36 -0800

    Add output display elements

--- src/farm.js
+++ src/farm.js
@@ -1,12 +1,12 @@
-export class Farm(name) {
-  constructor(age) {
-    this.name = name;
-    this.type = type;
-    this.preferredRace = race;
-    this.preferredEffects = [];
-    this.preferredFlavors = [];
-  }
+// export class Farm(name) {
+//   constructor(age) {
+//     this.name = name;
+//     this.type = type;
+//     this.preferredRace = race;
+//     this.preferredEffects = [];
+//     this.preferredFlavors = [];
+//   }
 
 
 
-}
+// }
--- src/main.js
+++ src/main.js
@@ -5,15 +5,21 @@ import './styles.css';
 // import { Farm } from './person';
 import { StrainService } from './strain-service';
 
+function showStrains(array) {
+  for (let i=0; i<=array.length; i++) {
+    $("#output").append(array[i].name + ", ");
+  }
+}
+
+
 $(document).ready(function() {
   $("#flavorlist").submit(function() {
     event.preventDefault();
     let currentFlav = $("#selectflavor option:selected").text();
-    console.log(currentFlav);
     (async () => {
       let strainService = new StrainService();
       const response = await strainService.getStrainbyFlavor(currentFlav);
-      console.log(response);
+      showStrains(response);
     })();
   });
 });
