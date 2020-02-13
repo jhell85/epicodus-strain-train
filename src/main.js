5cf57bfc02f2dc5ea7cdf907aa049ba2873436c9
Geoff Goetz, Joshua Hellman and Joseph Wangemann <joshua@>
2020-02-13 11:19:32 -0800

    Add additional error catching and display message for no results.

--- src/main.js
+++ src/main.js
@@ -4,26 +4,33 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 import './styles.css';
 import { StrainService } from './strain-service';
 
-const selectedArray = [];
+let selectedArray = [];
 function showStrains(array, race, flavor) {
+  console.log(array);
   for (let i=0; i<array.length; i++) {
     if (array[i].race == race && array[i].flavor == flavor) {
-      selectedArray.push(array[i].name + ", ");
+      selectedArray.push(" " + array[i].name);
     } 
   }
   console.log(selectedArray);
+  
+  $("#output").text(selectedArray);
 }
 
 
 $(document).ready(function() {
   $("form").submit(function() {
     event.preventDefault();
+    selectedArray = [];
     let currentRace = $("#selectrace").val();
     let currentFlav = $("#selectflavor option:selected").text();
     (async () => {
       let strainService = new StrainService();
       const response = await strainService.getStrain(currentFlav);
-      showStrains(response, currentRace, currentFlav); 
+      if(response.length === 0) {
+        $("#output").text("This is not the weed you're looking for.")
+      } else 
+        showStrains(response, currentRace, currentFlav); 
     })();
   });
 });
--- src/strain-service.js
+++ src/strain-service.js
@@ -1,11 +1,17 @@
 export class StrainService {
   async getStrain(flavor) {
     try {
-      let response = await fetch(`http://strainapi.evanbusse.com/${process.env.API_KEY}/strains/search/flavor/${flavor}`);
+      let response = await fetch(`strainapi.evanbusse.com/${process.env.API_KEY}/strains/search/flavor/${flavor}`);
+      console.log(response);
+      if (response.status != 200) {
+        throw Error("I'm sorry stoner, I can't let you do that.")
+      }
       let jsonifiedResponse = await response.json();
+      console.log(response.status);
       return jsonifiedResponse;
     } catch(error) {
       console.warn("There was a problem.... ewps!");
+      console.log(error)
     }
   }
 }
