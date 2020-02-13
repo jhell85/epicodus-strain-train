b4981ac3120502798ff53299ec2c64f243547028
Geoff Goetz, Joseph Wangemann and Joshua Hellman <geoff+joseph+joshua@>
2020-02-13 15:13:01 -0800

    add api call by name, add click function for li

--- src/main.js
+++ src/main.js
@@ -4,33 +4,38 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 import './styles.css';
 import { StrainService } from './strain-service';
 
-let selectedArray = [];
 function showStrains(array, race, flavor) {
-  // console.log(array);
   for (let i=0; i<array.length; i++) {
     if (array[i].race == race && array[i].flavor == flavor) {
-      selectedArray.push(" " + array[i].name);
+      $("#strainOut").append(`<li class="strainlist" value="`+array[i].name+`">` + array[i].name + `</li>`);
     } 
   }
-  // console.log(selectedArray);
-  
-  $("#output").text(selectedArray);
 }
 
 
 $(document).ready(function() {
   $("form").submit(function() {
     event.preventDefault();
-    selectedArray = [];
     let currentRace = $("#selectrace").val();
     let currentFlav = $("#selectflavor option:selected").text();
     (async () => {
       let strainService = new StrainService();
       const response = await strainService.getStrain(currentFlav);
       if(response.length === 0) {
-        $("#output").text("This is not the weed you're looking for.")
+        $("#output").text("This is not the weed you're looking for.");
       } else 
-        showStrains(response, currentRace, currentFlav); 
+      showStrains(response, currentRace, currentFlav); 
+    })();
+  });
+  $("ul#strainOut").on("click", "li", function() {
+    let currentStrain = $(this).attr("value");
+    (async () => {
+      let strainService = new StrainService();
+      const responseFinal = await strainService.getInfo(currentStrain);
+      if(responseFinal.length === 0) {
+        $("#output").text("This is not the weed you're looking for.");
+      } else 
+      console.log(responseFinal); 
     })();
   });
 });
--- src/strain-service.js
+++ src/strain-service.js
@@ -2,7 +2,6 @@ export class StrainService {
   async getStrain(flavor) {
     try {
       let response = await fetch(`https://strainapi.evanbusse.com/${process.env.API_KEY}/strains/search/flavor/${flavor}`);
-      console.log(response.status);
       let jsonifiedResponse;
       if (response.status == 200 && response.ok) {
         jsonifiedResponse = await response.json();
@@ -12,7 +11,23 @@ export class StrainService {
       return jsonifiedResponse;
     } catch(error) {
       console.warn("There was a problem.... ewps!");
-      console.log(error)
     }
   }
+ 
+  async getInfo(name) {
+    try {
+      let response = await fetch(`https://strainapi.evanbusse.com/${process.env.API_KEY}/strains/search/name/${name}`);
+      let jsonifiedResponse;
+      if (response.status == 200 && response.ok) {
+        jsonifiedResponse = await response.json();
+      } else {
+        jsonifiedResponse = false;
+      }
+      return jsonifiedResponse;
+    } catch(error) {
+      console.warn("There was a problem.... ewps!");
+    }
+  }
+
+
 }
